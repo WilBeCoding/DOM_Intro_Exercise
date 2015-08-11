c8300dafe28994fee9fc4d960679bef867199d8f
Wilson Rondini <wilbecoding@gmail.com>
2015-08-11 12:33:43 -0600

    Dodged a bullet

--- source.js
+++ source.js
@@ -1,8 +1,9 @@
 var essentialsSelection = document.getElementById("essentials");
 var milkSelection = essentialsSelection.firstElementChild.firstElementChild;
 var wineSelection = milkSelection.nextElementSibling.nextElementSibling.nextElementSibling;
+var beerSelection = wineSelection.nextElementSibling;
 
-
+console.log(milkSelection);
 console.log(wineSelection);
 
 milkSelection.addEventListener("click",function() {
@@ -10,3 +11,9 @@ milkSelection.addEventListener("click",function() {
 
 wineSelection.addEventListener("click", function() {
   wineSelection.style.color = 'red'});
+
+beerSelection.addEventListener("click", function() {
+var imageToAdd = document.createElement("img"); 
+imageToAdd.src = "Cage.gif"; 
+essentialsSelection.appendChild(imageToAdd);
+})
